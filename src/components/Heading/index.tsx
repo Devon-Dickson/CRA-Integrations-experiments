6d965caecbf7adceccce467b05966f7e8a8421e2
Devon Dickson <devon64327@gmail.com>
2020-10-23 19:40:20 -0400

    Add Heading and Section components
    Using the React Context API, we can automatically handle the iteration
    of Heading levels (h1, h2, etc). The clampHeadingLevel function will
    prevent us from accidentally declaring a heading level above h6, which
    is not supported by the HTML spec.
    
    It would be nice to enforce the 1 single h1 per page rule, but that
    would require some kind of static analysis.

---
+++ src/components/Heading/index.tsx
@@ -0,0 +1,51 @@
+import React, {
+    DetailedHTMLProps,
+    HTMLAttributes,
+    ReactNode,
+    useContext,
+} from "react";
+
+export const HeadingContext = React.createContext(1);
+
+type SectionProps = {
+    value?: number;
+    children: ReactNode;
+};
+
+export const Section = (props: SectionProps) => {
+    const headingContext = useContext(HeadingContext);
+    const headingLevel = clampHeadingLevel(headingContext + 1);
+    return (
+        <HeadingContext.Provider value={headingLevel}>
+            {props.children}
+        </HeadingContext.Provider>
+    );
+};
+
+export const Heading = (
+    props: DetailedHTMLProps<
+        HTMLAttributes<HTMLHeadingElement>,
+        HTMLHeadingElement
+    >
+) => {
+    const headingContext = useContext(HeadingContext);
+    const headingLevel = clampHeadingLevel(headingContext);
+    const Component = `h${headingLevel}`;
+    return <Component {...props}>{props.children}</Component>;
+};
+
+/**
+ * Clamps the headingLevel between 1 and 6 per the {@link https://html.spec.whatwg.org/multipage/sections.html#the-h1,-h2,-h3,-h4,-h5,-and-h6-elements HTML5 spec}:
+ *
+ * @param headingLevel - The heading level
+ * @returns The heading level clamped between 1 and 6
+ */
+export const clampHeadingLevel = (headingLevel: number): number => {
+    if (headingLevel > 6) {
+        return 6;
+    } else if (headingLevel < 1) {
+        return 1;
+    } else {
+        return headingLevel;
+    }
+};
