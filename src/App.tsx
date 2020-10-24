23485236a76dee9ddcf4094e4fe2e09aa9a2b22e
Devon Dickson <devon64327@gmail.com>
2020-10-24 09:53:42 -0400

    Another change of travis

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,12 @@
 import React from "react";
 import logo from "./logo.svg";
 import "App.css";
-import { Heading, Section } from "components/Heading";
 
 function App() {
     return (
         <div className="App">
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
-                <Heading>Heading 1</Heading>
-                <Section>
-                    <Heading>Heading 2</Heading>
-                    <Section>
-                        <Heading>Heading 3</Heading>
-                        <Heading>Heading 3</Heading>
-                        <Section>
-                            <Heading>Heading 4</Heading>
-                            <Section>
-                                <Heading>Heading 5</Heading>
-                                <Section>
-                                    <Heading>Heading 6</Heading>
-                                    <Section>
-                                        <Heading>Heading 6</Heading>
-                                        <Section>
-                                            <Heading>Heading 6</Heading>
-                                            <Section>
-                                                <Heading>Heading 6</Heading>
-                                                <Section>
-                                                    <Heading>Heading 6</Heading>
-                                                </Section>
-                                            </Section>
-                                        </Section>
-                                    </Section>
-                                </Section>
-                                <Heading>Heading 5</Heading>
-                                <Heading>Heading 5</Heading>
-                            </Section>
-                        </Section>
-                        <Heading>Heading 3</Heading>
-                    </Section>
-                </Section>
-
                 <a
                     className="App-link"
                     href="https://reactjs.org"
