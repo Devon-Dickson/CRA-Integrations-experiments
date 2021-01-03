b4742d54a3c13ae3b32de112ac4e00620bcc698f
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-01-03 18:22:06 +0000

    Deploy Devon-Dickson/my-app to github.com/Devon-Dickson/my-app.git:gh-pages

---
+++ precache-manifest.5620675729cc41f8deb6317a8839b8f9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "337d9e8790feadc06329bc9484e55747",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "a93a805206a16d105414",
+    "url": "/my-app/static/css/main.541524e8.chunk.css"
+  },
+  {
+    "revision": "20f9ca2bd46e5809ba7a",
+    "url": "/my-app/static/js/2.af5707af.chunk.js"
+  },
+  {
+    "revision": "8da57ccee1690fd1d0790b5fcb8f1bad",
+    "url": "/my-app/static/js/2.af5707af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a93a805206a16d105414",
+    "url": "/my-app/static/js/main.a47f43ff.chunk.js"
+  },
+  {
+    "revision": "833d64ac4bae055be974",
+    "url": "/my-app/static/js/runtime-main.58a6d01e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
